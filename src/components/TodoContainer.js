c4105d5abb5e238c408151cfeb3c33466f508f33
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-09-21 04:23:00 +0100

    added function to handle change of complted status

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,6 @@
-import React from "react";
-import TodosList from "./TodosList";
-import Header from "./Header";
+import React from 'react';
+import TodosList from './TodosList';
+import Header from './Header';
 
 class TodoContainer extends React.Component {
 
@@ -8,29 +8,40 @@ class TodoContainer extends React.Component {
     todos: [
       {
         id: 1,
-        title: "Setup development environment",
-        completed: true
+        title: 'Setup development environment',
+        completed: true,
       },
       {
         id: 2,
-        title: "Develop website and add content",
-        completed: false
+        title: 'Develop website and add content',
+        completed: true,
       },
       {
         id: 3,
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
-   };
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
 
-   render() {
+  handleChange = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <TodosList todos={this.state.todos} handleChangeProps={this.handleChange}/>
       </div>
     );
   }
 }
-export default TodoContainer
+export default TodoContainer;
